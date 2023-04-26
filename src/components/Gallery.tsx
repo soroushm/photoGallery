953582192159cf14c0a894ba8f275bdad2fc9261
Masoud Soroush <masoud.s.geo@gmail.com>
2023-04-26 22:14:26 +0200

    feat: add image Preview

--- src/components/Gallery.tsx
+++ src/components/Gallery.tsx
@@ -1,7 +1,19 @@
-import React, {useCallback, FC} from 'react';
+import React, {useCallback, FC, useState} from 'react';
 import {usePhotosQuery} from '../query/usePhotosQuery';
-import {Card, Image, PhotoList, PhotoListProps} from '../theme';
-import {ListRenderItemInfo} from 'react-native';
+import {
+  Card,
+  Image,
+  PhotoList,
+  PhotoListProps,
+  Modal,
+  Typography,
+} from '../theme';
+import {
+  ListRenderItemInfo,
+  TouchableOpacity,
+  Dimensions,
+  StyleSheet,
+} from 'react-native';
 import {Photo} from '../type';
 
 type GalleryProps = Omit<
@@ -11,14 +23,23 @@ type GalleryProps = Omit<
 
 export const Gallery: FC<GalleryProps> = props => {
   const {data, fetchNextPage, isFetching, refetch} = usePhotosQuery();
+  const [modalData, setModalData] = useState<Photo | null>(null);
   const images = data?.pages || [];
   const renderItem = useCallback(({item}: ListRenderItemInfo<Photo>) => {
-    return <Image source={{uri: item.thumbnailUrl}} />;
+    return (
+      <TouchableOpacity
+        style={styles.imageContainer}
+        onPress={() => setModalData(item)}>
+        <Image source={{uri: item.thumbnailUrl}} />
+      </TouchableOpacity>
+    );
   }, []);
 
   const extractKey = useCallback(item => {
     return item.id;
   }, []);
+
+  const onClose = () => setModalData(null);
 
   return (
     <Card flex={1} alignItems="stretch" justifyContent="center">
@@ -32,6 +53,20 @@ export const Gallery: FC<GalleryProps> = props => {
         onEndReached={() => fetchNextPage()}
         refreshing={isFetching}
       />
+      <Modal visible={!!modalData} onRequestClose={onClose}>
+        <Card height={Dimensions.get('window').width - 32} mb="md">
+          <Typography flex={1} pb="sm" fontWeight="bold">
+            {modalData?.title}
+          </Typography>
+          <Image source={{uri: modalData?.url}} />
+        </Card>
+      </Modal>
     </Card>
   );
 };
+
+const styles = StyleSheet.create({
+  imageContainer: {
+    flex: 1,
+  },
+});
